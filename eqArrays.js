686e5c766ee937fa00952a9abdba4a01cd79bb2f
megan <meganambermcbride@gmail.com>
2022-05-31 21:12:44 -0400

    Loops through the arrays instead of stringify

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,15 @@
 const eqArrays = function(array1, array2) {
-  if(JSON.stringify(array1) === JSON.stringify(array2)) {
-    return true
-  } else {
-    return false
+  if (array1.length !== array2.length) {
+    return false;
   }
-}
+
+  for (let i = 0; i < array1.lenght; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
@@ -18,7 +23,7 @@ const assertEqual = function(actual, expected) {
 
 
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false))
+console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
 
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true))
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false))
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));
