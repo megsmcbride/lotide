dbb1fac371c76c3394343082ce43b929b161d363
megan <meganambermcbride@gmail.com>
2022-05-31 21:46:38 -0400

    Fixed bugs

--- eqArrays.js
+++ eqArrays.js
@@ -3,13 +3,13 @@ const eqArrays = function(array1, array2) {
     return false;
   }
 
-  for (let i = 0; i < array1.lenght; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
+  for (let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[i]) {
+      return false
     }
   }
-  return true;
-};
+  return true
+}
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
