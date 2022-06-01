7fa6f335d8c015e4c018ad5020e68b67c3989d99
megan <meganambermcbride@gmail.com>
2022-06-01 14:24:49 -0400

    Removes specified element from a given array

---
+++ without.js
@@ -0,0 +1,38 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return;
+  }
+  console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  
+
+};
+
+
+const without = function(source, itemsToRemove) {
+  let output = []
+  for(let number of source) {
+    if(!itemsToRemove.includes(number)) {
+      output.push(number)
+    }
+  }
+  return output
+}
+
+
+console.log(without([1, 2, 3], [1]))// => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
