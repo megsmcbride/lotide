3ba83acdfbad2413e9b6d41b45db5bfb192d26ac
megan <meganambermcbride@gmail.com>
2022-06-01 09:20:31 -0400

    Fixed formating

--- eqArrays.js
+++ eqArrays.js
@@ -4,12 +4,12 @@ const eqArrays = function(array1, array2) {
   }
 
   for (let i = 0; i < array1.length; i++) {
-    if(array1[i] !== array2[i]) {
-      return false
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
