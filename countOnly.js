cc67f6234addefc5c5b407dc1cba1084999c0db7
megan <meganambermcbride@gmail.com>
2022-06-01 18:41:54 -0400

    Takes an array and returns the count for a specific set of items in an object

---
+++ countOnly.js
@@ -0,0 +1,47 @@
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+  
+    
+    if (itemsToCount[item] === true && results[item] === undefined) { 
+      results[item] = 0; 
+    }
+    
+    if(itemsToCount[item] === true){
+      results[item]++;
+    }
+  }
+  return results;
+}
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+
+};
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
