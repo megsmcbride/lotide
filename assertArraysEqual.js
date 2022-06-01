4903680162c4fcfa5dbbd60f23fd80208aeab43f
megan <meganambermcbride@gmail.com>
2022-06-01 14:24:12 -0400

    Fixed formating

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const actual = function(array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
@@ -12,18 +12,18 @@ const actual = function(array1, array2) {
 };
 
 
-const assertArraysEqual = function(actual,expected) {
+const assertArraysEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
     return;
   }
   console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  
+
 
 };
 
-console.log(assertArraysEqual(actual([1, 2, 3], [1, 2, 3]), true));
-console.log(assertArraysEqual(actual([1, 2, 3], [3, 2, 1]), false));
+console.log(assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+console.log(assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
 
-console.log(assertArraysEqual(actual(["1", "2", "3"], ["1", "2", "3"]), true));
-console.log(assertArraysEqual(actual(["1", "2", "3"], ["1", "2", 3]), false));
+console.log(assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
+console.log(assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));
