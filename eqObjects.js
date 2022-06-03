ae21ca57386d2713f77a590573a9e392a9bfa3fd
megan <meganambermcbride@gmail.com>
2022-06-03 00:24:56 -0400

    Allowed assertEqual to callback eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -1,8 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+const assertEqual = function(object1, object2) {
+  if (eqObjects(object1, object2)) {
+    console.log(`✅✅✅ Assertion Passed: ${object1} === ${object2}`);
+  } else if (object1 !== object2) {
+    console.log(`❌❌❌ Assertion Failed: ${object1} !== ${object2}`);
 
   }
 
@@ -26,7 +26,7 @@ const eqObjects = function(object1, object2) {
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
+assertEqual(ab, ba); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
+assertEqual(ab, abc) // => false
