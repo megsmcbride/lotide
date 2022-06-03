351a453de1b3d794cffa1b2830e280ca04c8fa42
megan <meganambermcbride@gmail.com>
2022-06-02 23:08:08 -0400

    Fixed AsserArraysEquals so it now calls back eqArrays

--- map.js
+++ map.js
@@ -3,7 +3,7 @@ const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
-  
+
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
@@ -14,27 +14,27 @@ const eqArrays = function(array1, array2) {
 
 
 const assertArraysEqual = function(actual, expected) {
-  if (actual === expected) {
+  if (eqArrays(array1, array2)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
     return;
   }
   console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  
-  
+
+
 };
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
   const results = [];
- for(let item of array) {
-   results.push(callback(item));
- }
+  for (let item of array) {
+    results.push(callback(item));
+  }
   return results;
 };
 
 
 
 const results1 = map(words, word => word[0]);
-console.log(assertArraysEqual(eqArrays(results1, [ 'g', 'c', 't', 'm', 't' ]), true));
-console.log(assertArraysEqual(eqArrays(results1, [ 'g', 'c', 't', 'm', 'p' ]), false));
-console.log(assertArraysEqual(eqArrays(results1, [ 'g', 'c', 't', 'm', 'p', 'w' ]), false));
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 'p']);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 'p', 'w']);
