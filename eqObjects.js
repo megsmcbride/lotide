e032820ce47742051b959f20f5e14bf43674248f
megan <meganambermcbride@gmail.com>
2022-06-03 00:14:49 -0400

    Compares two objects and returns true of false if match

---
+++ eqObjects.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+
+  for (let key of Object.keys(object1)) {
+    if (object1[`${key}`] === object2[`${key}`]) {
+
+    }
+
+  }
+
+  return true;
+
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
