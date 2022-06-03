f2407c874d3ce780a48085ebaf43b010519afb8d
megan <meganambermcbride@gmail.com>
2022-06-03 00:11:36 -0400

    Fixed assertArraysEqual function and bugs

--- letterPositions.js
+++ letterPositions.js
@@ -1,7 +1,29 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+    return;
+  }
+  console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+
+
+};
 const letterPositions = function(sentence) {
   const results = {};
   let lowerCaseSentence = sentence.toLowerCase();
-  console.log(lowerCaseSentence);
   for (let i = 0; i < lowerCaseSentence.length; i++) {
     if (lowerCaseSentence[i] === ' ') {
       continue;
@@ -14,37 +36,14 @@ const letterPositions = function(sentence) {
     results[lowerCaseSentence[i]].push(i);
 
   }
-  console.log(results);
   return results;
 
 
 };
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
 
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-
-const assertArraysEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    return;
-  }
-  console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-
-
-};
 
 //Test Case
-console.log(assertArraysEqual(letterPositions("hello").e, [2]));
-console.log(assertArraysEqual(letterPositions("LightHouse in the House").h, [3, 5, 15, 18]));
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("LightHouse in the House").h, [3, 5, 15, 18]);
+assertArraysEqual(letterPositions("nothing founed").e, [2]);
