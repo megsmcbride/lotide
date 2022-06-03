3cc5a79bf9bfd035fcf0a05f2511e216441a2017
megan <meganambermcbride@gmail.com>
2022-06-03 10:02:18 -0400

    Reformated by removing else statement

--- assertEqual.js
+++ assertEqual.js
@@ -2,12 +2,13 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-
-  }
+    return
+  } 
+  
+  console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 
 };
+
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
