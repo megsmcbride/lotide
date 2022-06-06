799e64e20d8d265983a04f90c9b214f65f4394ca
megan <meganambermcbride@gmail.com>
2022-06-06 16:55:25 -0400

    Fixed formate

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 const assertEqual = ((actual, expected) => {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    return;
+    return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
   }
 
-  console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  return (`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 
 });
 
