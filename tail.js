a4ee5b3f1015104a412b32cbfe6e0f834b0744ff
megan <meganambermcbride@gmail.com>
2022-06-06 14:10:27 -0400

    Refactored

--- tail.js
+++ tail.js
@@ -1,28 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-
-};
-
-const tail = function(element) {
+const tail = (element => {
   let output = element.slice(1);
   console.log(output);
-};
+});
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-
-const words1 = [];
-tail(words1);
-assertEqual(words1.length, 3);
-
-const words2 = [1];
-tail(words2);
-assertEqual(words2.length, 3);
+module.exports = tail
