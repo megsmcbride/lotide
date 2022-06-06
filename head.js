988bab2c4bf82df26231fcc02ce9ce1f3f209f5e
megan <meganambermcbride@gmail.com>
2022-06-06 13:52:47 -0400

    Refactored

--- head.js
+++ head.js
@@ -1,23 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-
-};
-
-const head = function(element) {
+const assertEqual = require('./assertEqual')
+const head = (element => {
   return element[0]
-}
+})
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
-assertEqual(head(["Labs"]), "Hello");
+module.exports = head
